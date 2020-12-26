d5c05c84038ff9a130c68e9f7bcfb2aaed2b071d
PeterDoom <nanev0205@gmail.com>
2020-12-26 22:21:26 +0200

    A mandatory Hello World!

---
+++ src/main/java/com/springboot/demo/restwebservice/demo/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.restwebservice.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+}
