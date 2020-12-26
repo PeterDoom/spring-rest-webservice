353fd0e1cba26a1756f7742e7435a49117d4a52b
PeterDoom <nanev0205@gmail.com>
2020-12-26 22:53:54 +0200

    Adding a get request mapping for Hello World endpoint that returns a bean

--- src/main/java/com/springboot/demo/restwebservice/demo/HelloWorldController.java
+++ src/main/java/com/springboot/demo/restwebservice/demo/HelloWorldController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello-world")
+    @GetMapping("/")
     public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
 }
