fd64a3cc78e73cce4645c05c3eec5ee1662de0b3
PeterDoom <nanev0205@gmail.com>
2020-12-27 17:10:04 +0200

    Adding 201 Created Status response to the Post Request

--- src/main/java/com/springboot/demo/restwebservice/demo/user/UserResource.java
+++ src/main/java/com/springboot/demo/restwebservice/demo/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.springboot.demo.restwebservice.demo.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,8 +24,15 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User savedUser = userDAO.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
