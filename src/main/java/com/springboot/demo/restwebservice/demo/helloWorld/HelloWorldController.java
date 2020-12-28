c7968c9c9804bef0e430a6aae3af7b42dd8044ae
PeterDoom <nanev0205@gmail.com>
2020-12-28 22:24:38 +0200

    Removing unused imports

--- src/main/java/com/springboot/demo/restwebservice/demo/helloWorld/HelloWorldController.java
+++ src/main/java/com/springboot/demo/restwebservice/demo/helloWorld/HelloWorldController.java
@@ -2,14 +2,10 @@ package com.springboot.demo.restwebservice.demo.helloWorld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
