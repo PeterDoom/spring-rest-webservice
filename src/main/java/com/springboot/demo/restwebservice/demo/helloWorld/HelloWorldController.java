37dca2dc0a2b4c0d8075e886d7e0617c7893b57a
PeterDoom <nanev0205@gmail.com>
2020-12-28 21:35:50 +0200

    Adding internationalization to the hello world path

--- src/main/java/com/springboot/demo/restwebservice/demo/helloWorld/HelloWorldController.java
+++ src/main/java/com/springboot/demo/restwebservice/demo/helloWorld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.springboot.demo.restwebservice.demo.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/")
     public String helloWorld() {
@@ -16,8 +24,14 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
+
     @GetMapping("/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean("Hello " + name);
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
