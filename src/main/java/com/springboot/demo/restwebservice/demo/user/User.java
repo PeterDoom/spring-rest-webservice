97f933c33b68c39ccb10ef225c2512e026ff246b
PeterDoom <nanev0205@gmail.com>
2021-01-11 23:42:21 +0200

    Making user an entity

--- src/main/java/com/springboot/demo/restwebservice/demo/user/User.java
+++ src/main/java/com/springboot/demo/restwebservice/demo/user/User.java
@@ -2,13 +2,19 @@ package com.springboot.demo.restwebservice.demo.user;
 
 import io.swagger.annotations.ApiModel;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ApiModel(description = "Demo description for the user")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
@@ -21,6 +27,10 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    protected User() {
+
     }
 
     public Integer getId() {
