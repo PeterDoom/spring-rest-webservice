6da0747728adf67698bfba2441231b26c3684bcb
PeterDoom <nanev0205@gmail.com>
2021-01-19 23:15:50 +0200

    Adding user repository interface + a JPA resource

---
+++ src/main/java/com/springboot/demo/restwebservice/demo/user/UserJPAResource.java
@@ -0,0 +1,63 @@
+package com.springboot.demo.restwebservice.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserJPAResource {
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/jpa/users")
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/jpa/users/{id}")
+    public EntityModel<User> retrieverUserById(@PathVariable int id) {
+        User user = userDAO.fin(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        EntityModel<User> resource = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
+    }
+
+    @PostMapping("/jpa/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = userDAO.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = userDAO.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+    }
+
+}
