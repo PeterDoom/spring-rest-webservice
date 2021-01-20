45851d8b15dc67a7f3b8227f49eb5374e7cf91bf
PeterDoom <nanev0205@gmail.com>
2021-01-20 23:50:25 +0200

    Adding an insert into the data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values(1, sysdate(), 'Aby');
 insert into user values(2, sysdate(), 'hello');
 insert into user values(3, sysdate(), 'Jake');
+
+insert into post values(11001, 'MY FIRST POST', 1)
