2a65ba099739170031a2eddad7599314e3584ff4
PeterDoom <nanev0205@gmail.com>
2021-01-20 23:44:03 +0200

    Adding OneToMany and ManyToOne connection for Post-User relationship

--- src/main/java/com/springboot/demo/restwebservice/demo/user/Post.java
+++ src/main/java/com/springboot/demo/restwebservice/demo/user/Post.java
@@ -1,11 +1,16 @@
 package com.springboot.demo.restwebservice.demo.user;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Post {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     public Integer getId() {
